a71862d13074fe80e4b14c5d69bbea2e08392b60
vishalkrsharma <vishalkrsharma1234@gmail.com>
2022-09-04 20:49:19 +0530

    bg added, pan not working

--- src/components/Model.js
+++ src/components/Model.js
@@ -1,15 +1,16 @@
-import React from 'react';
-import { Canvas, useLoader } from '@react-three/fiber';
-import { OrbitControls, Stars } from '@react-three/drei';
-import { Physics, useSphere } from '@react-three/cannon';
+import React, { Suspense } from 'react';
+import { Canvas, useLoader, useThree } from '@react-three/fiber';
+import { OrbitControls } from '@react-three/drei';
+import * as THREE from 'three';
+// import { Physics, useSphere } from '@react-three/cannon';
 
-import texture from '../assets/moon.jpg';
+import moon from '../assets/moon.jpg';
+import stars from '../assets/stars.jpg';
 
 import '../styles/Model.scss';
-import { TextureLoader } from 'three';
 
 function Shape() {
-  const moonTexture = useLoader(TextureLoader, texture);
+  const moonTexture = useLoader(THREE.TextureLoader, moon);
 
   return (
     <mesh rotation={[0, -Math.PI / 5, 0]}>
@@ -19,15 +20,28 @@ function Shape() {
   );
 }
 
+function Space() {
+  const { scene } = useThree();
+
+  const starsTexture = useLoader(THREE.TextureLoader, stars);
+
+  THREE.Texture.mapping = THREE.EquirectangularRefractionMapping;
+
+  scene.background = starsTexture;
+  return null;
+}
+
 function Model() {
   return (
     <div className='model'>
       <Canvas className='canvas' camera={{ fov: 35, zoom: 1.0, near: 1, far: 1000 }}>
-        <OrbitControls />
-        <Stars />
-        <ambientLight intensity={0.03} />
-        <spotLight position={[90, 0, 90]} intensity={1} angle={0.5} />
-        <Shape />
+        <OrbitControls enableZoom={true} enablePan={true} />
+        <Suspense>
+          <ambientLight intensity={0.03} />
+          <spotLight position={[90, 0, 90]} intensity={1} angle={0.5} />
+          <Space />
+          <Shape />
+        </Suspense>
       </Canvas>
     </div>
   );
