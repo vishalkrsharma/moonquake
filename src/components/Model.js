d4f4999a0fb1b1168225164493891d5a80a5f4ac
maaz-shahid-99 <goluthakle@gmail.com>
2022-09-06 19:16:29 +0530

    added circle.png to assets

--- src/components/Model.js
+++ src/components/Model.js
@@ -1,11 +1,12 @@
-import React, { Suspense, useState } from 'react';
-import { Canvas } from '@react-three/fiber';
-import { OrbitControls } from '@react-three/drei';
+import React, { Suspense, useState } from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
 
-import Moon from './Moon';
-import Menu from './Menu';
+import Moon from "./Moon";
+import Menu from "./Menu";
 
-import '../styles/Model.scss';
+import "../styles/Model.scss";
+import Points from "./Point";
 
 function Model() {
   const [axes, setAxes] = useState(0);
@@ -14,18 +15,35 @@ function Model() {
 
   return (
     <>
-      <div className='model'>
-        <Canvas className='canvas' camera={{ fov: 35, zoom: 1, near: 1, far: 1000 }}>
+      <div className="model">
+        <Canvas
+          className="canvas"
+          camera={{ fov: 35, zoom: 1, near: 1, far: 1000 }}
+        >
           <OrbitControls enableZoom={true} />
           <Suspense>
             <ambientLight intensity={0.03} />
-            <spotLight position={[10, 0, 10]} intensity={spotLightIntensity / 100} angle={0.5} />
+            <spotLight
+              position={[10, 0, 10]}
+              intensity={spotLightIntensity / 100}
+              angle={0.5}
+            />
             <Moon />
             <axesHelper args={[axes, 50, 50]} position={[0, 0, 0]} />
           </Suspense>
         </Canvas>
       </div>
-      <Menu spotLightIntensity={spotLightIntensity} setSpotLightIntensity={setSpotLightIntensity} axes={axes} setAxes={setAxes} />
+      <div>
+        <Canvas>
+          <Points />
+        </Canvas>
+      </div>
+      <Menu
+        spotLightIntensity={spotLightIntensity}
+        setSpotLightIntensity={setSpotLightIntensity}
+        axes={axes}
+        setAxes={setAxes}
+      />
     </>
   );
 }
