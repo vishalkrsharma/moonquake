3d74ce709b2c685caa0360497631f4b0b0f13e17
vishalkrsharma <vishalkrsharma1234@gmail.com>
2022-09-08 23:15:56 +0530

    pre animation commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import '@fontsource/roboto-mono';
+import { Canvas } from '@react-three/fiber';
+import { Suspense } from 'react';
 
 import Model from './components/Model';
-
 import './App.scss';
 
 function App() {
--- src/components/Wireframe.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-function Wireframe() {
-  const gltf = useLoader;
-  return <mesh></mesh>;
-}
-
-export default Wireframe;
