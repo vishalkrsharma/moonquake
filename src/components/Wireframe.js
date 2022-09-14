b95e054038f33153dbc035ad1d87b9bdedd650b0
maaz-shahid-99 <goluthakle@gmail.com>
2022-09-14 14:54:34 +0530

    wireframe fixed

--- src/components/Wireframe.js
+++ src/components/Wireframe.js
@@ -1,8 +1,8 @@
-import { useLoader } from '@react-three/fiber';
-import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader';
+import { useLoader } from "@react-three/fiber";
+import { FBXLoader } from "three/examples/jsm/loaders/FBXLoader";
 function Wireframe() {
-  const fbx = useLoader(FBXLoader, '/Wireframe.fbx');
-  return <primitive object={fbx} scale={0.02005} />;
+  const fbx = useLoader(FBXLoader, "/wireframe.fbx");
+  return <primitive object={fbx} scale={0.01003} />;
 }
 
 export default Wireframe;
