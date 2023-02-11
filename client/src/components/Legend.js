d65bbbe2cb5549879a88bada78dabef68b002295
vishalkrsharma <vishalkrsharma1234@gmail.com>
2023-02-11 17:16:33 +0530

    env fixed

--- client/src/components/Legend.js
+++ client/src/components/Legend.js
@@ -6,8 +6,8 @@ import legend from '../assets/legend.png';
 function Legend() {
   return (
     <div className='legend'>
-      <div className='legend__top'>+1000km</div>
-      <div className='legend__bottom'>-1000km</div>
+      <div className='legend__top'>+1000m</div>
+      <div className='legend__bottom'>-1000m</div>
       <img src={legend} alt='' className='legend__img' />
     </div>
   );
